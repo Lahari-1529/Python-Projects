c1e9ff2c8e044398582f11355ae4f93ee0901367
Lahari-1529 <lahariluna@gmail.com>
2026-06-24 21:54:47 +0530

    First commit

---
+++ calculator.py
@@ -0,0 +1,17 @@
+def add(a,b):
+    return(a+b)
+def sub(a,b):
+    return(a-b)
+def multi(a,b):
+    return(a*b)
+def divi(a,b):
+    return(a/b)
+def mod(a,b):
+    return(a%b)
+a=int(input("Enter first number:"))
+b=int(input("Entersecond number:"))
+print("Add=",add(a,b))
+print("Subtract=",sub(a,b))
+print("Multiply=",multi(a,b))
+print("Divide=",divi(a,b))
+print("Mod=",mod(a,b))
